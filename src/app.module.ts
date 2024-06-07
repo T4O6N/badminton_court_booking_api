a6ccaf0311cae4a9487ce8c6dc657b93a206fc42
tontakarn <3t9o9n9@gmail.com>
2024-06-07 19:46:51 +0700

    add logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,47 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AcceptLanguageResolver, HeaderResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
+import * as path from 'path';
+import { PrismaModule } from './config/prisma/prisma.module';
+import { WinstonLoggerService } from './config/loggers/logger.service';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { LanguageModule } from './config/lang/language.module';
+import { CourtsModule } from './api/courts/courts.module';
+import { CourtBookingModule } from './api/court-bookings/court-bookings.module';
+import { StaffModule } from './api/staff/staff.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        I18nModule.forRoot({
+            fallbackLanguage: 'en',
+            loaderOptions: {
+                path: path.join(__dirname, './i18n'),
+                watch: true,
+                includeSubfolders: true,
+            },
+            resolvers: [
+                {
+                    use: QueryResolver,
+                    options: ['lang'],
+                },
+                AcceptLanguageResolver,
+                new HeaderResolver(['x-lang']),
+            ],
+        }),
+        PrismaModule,
+        LanguageModule,
+        CourtsModule,
+        CourtBookingModule,
+        StaffModule,
+    ],
+    controllers: [],
+    providers: [WinstonLoggerService],
 })
-export class AppModule {}
+export class AppModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(LoggerMiddleware).forRoutes('*');
+    }
+}
