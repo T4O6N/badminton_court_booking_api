1006fbe6f382c89bc9e36016fedb39e1a66f3e3d
tontakarn <3t9o9n9@gmail.com>
2024-06-16 16:11:26 +0700

    delete if it not important

--- src/api/courts/dto/update-court.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CourtDto } from './create-court.dto';
-
-export class UpdateCourtDto extends PartialType(CourtDto) {}
--- src/api/staff/dto/update-staff.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { StaffDto } from './create-staff.dto';
-
-export class UpdateStaffDto extends PartialType(StaffDto) {}
