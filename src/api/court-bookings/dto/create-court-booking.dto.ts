4eef1e5add75186507449eab61b86416fb0a44b7
tontakarn <3t9o9n9@gmail.com>
2024-06-16 16:18:34 +0700

    change filed book_duration in court booking model to book_expired

--- src/api/court-bookings/dto/create-court-booking.dto.ts
+++ src/api/court-bookings/dto/create-court-booking.dto.ts
@@ -4,7 +4,7 @@ export class CourtBookingDto {
     public phone: string;
     public first_name: string;
     public payment_Status: PaymentStatus;
-    public book_duration: number;
+    public book_expired: number;
     public booked_by: string;
     public total_amount: number;
     public court: Court[];
