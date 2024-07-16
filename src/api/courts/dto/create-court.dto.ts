0aa7eab6199bbeaaf6a71a71fe27b0bc5fdfd1a0
tontakarn <3t9o9n9@gmail.com>
2024-07-16 23:22:35 +0700

    update court model

--- src/api/courts/dto/create-court.dto.ts
+++ src/api/courts/dto/create-court.dto.ts
@@ -20,7 +20,7 @@ export class CourtDTO {
         description: 'court image',
         example: 'court image',
     })
-    court_image: string;
+    court_image: string[];
 
     @ApiProperty({
         required: true,
