3f7bdb4a162cddf1a537b22b4fd09ce5ceffd804
tontakarn <3t9o9n9@gmail.com>
2024-07-19 02:48:38 +0700

    change moment package back to original

--- src/api/court-bookings/court-bookings.service.ts
+++ src/api/court-bookings/court-bookings.service.ts
@@ -3,7 +3,7 @@ import { PrismaService } from 'src/config/prisma/prisma.service';
 import { CourtBookingDTO } from './dto/court-booking.dto';
 import { UpdateCourtDto } from '../courts/dto/update-court.dto';
 import { CourtBookingHistory } from '@prisma/client';
-import moment from 'moment';
+import * as moment from 'moment';
 
 @Injectable()
 export class CourtBookingsService {
